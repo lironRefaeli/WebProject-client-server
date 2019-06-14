495d85555246f1a90acbbcc3516c37790632c4a0
liron refaeli <lironref@post.bgu.ac.il>
2019-06-14 13:45:51 +0300

    display gallery

--- app/components/POI/POIinformation.controller.js
+++ app/components/POI/POIinformation.controller.js
@@ -10,6 +10,9 @@ angular.module('appModule').service('poiService', ['$http', function ($http) {
     let vm = this;
     vm.poiInfo = undefined;
     vm.percentageRank = undefined;
+    vm.critic = undefined;
+    vm.loadFirstCritic = false;
+    vm.loadSecondCritic = false;
     vm.criticDate = [];
     getPOIinformation();
 
@@ -18,6 +21,11 @@ angular.module('appModule').service('poiService', ['$http', function ($http) {
         poiService.getPOIinfo($routeParams.id).then(function (response) {
             vm.poiInfo = response.data;
             vm.percentageRank = parseInt(vm.poiInfo[0][0]['rank'])/5 * 100;
+
+            if(vm.poiInfo[1][0] !== undefined )
+                vm.loadFirstCritic = true;
+            if(vm.poiInfo[1][1]!== undefined )
+                vm.loadSecondCritic = true;
             vm.criticDate[0] = vm.poiInfo[1][0]['criticDate'].substring(0, 10);
             vm.criticDate[1] = vm.poiInfo[1][1]['criticDate'].substring(0, 10);
 
